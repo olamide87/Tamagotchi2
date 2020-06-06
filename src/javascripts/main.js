dfdb954baca70071fc3874c17dbc638934750281
Olamide Oladinni <olajide1987@gmail.com>
2020-06-06 11:43:01 -0500

    play

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,6 +1,8 @@
 import '../styles/main.scss';
 import eat from './components/eat/eat';
 import eatData from './helpers/data/eadData';
+import play from './components/play/play';
+import playData from './helpers/data/playData';
 
 const addToFullScore = () => {
   eatData.setFullScore(10);
@@ -12,14 +14,27 @@ const subFromFullScore = () => {
   eat.eatBuilder();
 };
 
+const addSuperFun = () => {
+  playData.setFunScore(50);
+  play.playBuilder();
+};
+
+const addSlightlyFun = () => {
+  playData.setFunScore(2);
+  play.playBuilder();
+};
+
 const buttonEvents = () => {
   $('#eat').on('click', '#healthy-food-btn', addToFullScore);
   $('#eat').on('click', '#unhealthy-food-btn', subFromFullScore);
+  $('#play').on('click', '#super-fun-btn', addSuperFun);
+  $('#play').on('click', '#slightly-fun-btn', addSlightlyFun);
 };
 
 
 const quad = () => {
   eat.eatBuilder();
+  play.playBuilder();
 };
 
 const init = () => {
